fba38bbb4d885ff6e96ea0fee880d6446e9ee315
mai.maegawa <mai.maru0214@icloud.com>
2022-03-05 09:25:16 +0900

    FrontController作成

--- src/main/java/constants/JpaConst.java
+++ src/main/java/constants/JpaConst.java
@@ -3,10 +3,10 @@ package constants;
  * DB関連の項目値を定義するインターフェース
  * ※インターフェイスに定義した変数は public static final 修飾子がついているとみなされる
  */
-public class JpaConst {
+public interface JpaConst {
 
     //persistence-unit名
-    String PERSISTENCE_UNIT_NAME = "daily_report_system";
+     String PERSISTENCE_UNIT_NAME = "daily_report_system";
 
     //データ取得件数の最大値
     int ROW_PER_PAGE = 15;//1ページに表示するレコードの数
